9538ab5c5528dc121d230de8a088226007dfd6f7
shubham singh <singhshubham68738@gmail.com>
2025-09-02 23:17:29 +0530

    working on pattern

---
+++ pattern/pattern.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+void main()
+{
+    int i, j;
+    // Question 1:-
+
+    //  for (i = 0; i < 5; i++)
+    //  {
+    //      for (j = 0; j <= i; j++)
+    //      {
+    //          printf("*");
+    //      }
+    //      printf("\n");
+    //  }
+
+    // Question 2:-
+
+    // for (i = 1; i < 6; i++)
+    // {
+    //     for (j = 1; j <= i; j++)
+    //     {
+    //         printf("%d", i);
+    //     }
+    //     printf("\n");
+    // }
+}
